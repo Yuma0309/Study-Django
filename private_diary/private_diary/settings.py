9fb94f271ab8fcb61842887990ae4850f499ced6
中村祐馬 <yuma.nakamura.0309@gmail.com>
2026-01-27 00:27:59 +0900

    settings.pyにosインポートとDB設定のデフォルト値を追加、.gitignoreを更新

--- private_diary/private_diary/settings.py
+++ private_diary/private_diary/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'private_diary',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': os.environ.get('DB_USER', 'yumanakamura'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': '',
         'PORT': '',
     }
