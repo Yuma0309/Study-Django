7c520bafd08ef00a74288869e9fefe5a5edf5df7
中村祐馬 <yuma.nakamura.0309@gmail.com>
2026-02-07 01:47:23 +0900

    リスト8.6までの動作を確認

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -5,6 +5,6 @@ from .forms import InquiryForm
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
-class InquiryView(generic.TemplateView):
+class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
